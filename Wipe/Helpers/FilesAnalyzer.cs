289cb5b78efb5e2751a1f666fe44f2b4c0da21b5
Mykyta Sikachyna <sikachyna.n@gmail.com>
2025-03-01 20:11:14 +0200

    feat: implement the FilesAnalyzer

---
+++ Wipe/Helpers/FilesAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿namespace Wipe.Helpers;
+
+internal class FilesAnalyzer
+{
+    private readonly string tempPath = Path.GetTempPath();
+
+    public async Task<long> AnalyzeFilesAsync()
+    {
+        return await Task.Run(() =>
+        {
+            if (!Directory.Exists(tempPath))
+                return 0;
+
+            return Directory.GetFiles(tempPath, "*", SearchOption.AllDirectories)
+                .Sum(file => new FileInfo(file).Length);
+        });
+    }
+}
